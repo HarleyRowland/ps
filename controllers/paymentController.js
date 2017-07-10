930f778ed90d505dd1276b74890d13b755976aed
Harley Rowland <harleyrowland@harleys-mbp.home>
2017-07-10 06:22:40 +0100

    Update queries

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,18 +1,42 @@
 module.exports = {
-	flow: function(view, req, callback){
-		console.log(view)
-		var viewFields = {
-			data: req.query
-		} 
-		if(view == "club.pug"){
-			viewFields.data.clubs = buildClubsArray()
-		} else if(view == "strip.pug"){
-			viewFields.data.clubs = howManyStrips(req.query.club)
-		} else if(view == "hero.pug"){
-			var nameNumberTuple = getPlayersAndNumbers(req.query.club)
-			viewFields.data.players = nameNumberTuple[0]
-			viewFields.data.numbers = nameNumberTuple[1]
-		}
-		callback(view, viewFields)
+	makePayment: function(view, req, callback){
+		let amount = 500;
+		stripe.customers.create({
+		 email: req.body.stripeEmail,
+		source: req.body.stripeToken
+		})
+		.then(customer =>
+		stripe.charges.create({
+		  amount,
+		  description: "Sample Charge",
+		     currency: "GBP",
+		     customer: customer.id
+		}))
+		.then(charge => {
+		var transporter = nodemailer.createTransport({
+		    service: 'Gmail',
+		    auth: {
+		        user: 'harleyrowland17@gmail.com', // Your email id
+		        pass: 'Omg_0923' // Your password
+		    }
+		});
+		var text = 'Payment made from \n\n Harley';
+		var mailOptions = {
+		  from: 'harleyrowland17@gmail.com', // sender address
+		  to: req.body.stripeEmail, // list of receivers
+		  subject: 'Email Example',
+		  html: fs.readFileSync('emailTemplates/orderProcessed.txt', 'utf8')
+		};
+		transporter.sendMail(mailOptions, function(error, info){
+		  if(error){
+		      console.log(error);
+		      res.json({yo: 'error'});
+		  }else{
+		      console.log('Message sent: ' + info.response);
+		      res.json({yo: info.response});
+		  };
+		});
+		res.render("paymentResult.pug", {finalCost: req.query.finalCost});
+		})
 	}
 }
--- queries.sql
+++ queries.sql
@@ -1,10 +1,23 @@
+DROP TABLE shirts;
+DROP TABLE statuses;
 DROP TABLE orders;
 
 CREATE TABLE orders(
    orderNumber serial primary key,
    email VARCHAR(400) NOT NULL,
-   status INT NOT NULL,
-   cost VARCHAR(400) NOT NULL,
+   cost INT NOT NULL
+);
+
+CREATE TABLE statuses(
+   statusID serial primary key,
+   orderNumber serial NOT NULL references orders(orderNumber),
+   dateChanged timestamp NOT NULL,
+   description VARCHAR(400) NOT NULL
+);
+
+CREATE TABLE shirts(
+   shirtID serial primary key,
+   orderNumber serial NOT NULL references orders(orderNumber),
    sleeve BOOLEAN NOT NULL,
    kitName VARCHAR(400) NOT NULL,
    kitNumber VARCHAR(400) NOT NULL,
@@ -17,17 +30,62 @@ CREATE TABLE orders(
    letter VARCHAR(400)
 );
 
-INSERT INTO orders(email, status, cost, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
-	VALUES ('test1@gmail.com', 0, '25', false, 'Kolarov', '11', 'post', 'hero', 'current', 'manchesterCity', 'home');
+INSERT INTO orders(email, cost) 
+   VALUES ('test@gmail.com', 25);
 
-INSERT INTO orders(email, status, cost, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
-	VALUES ('test1@gmail.com', 0, '25', false, 'Sterling', '7', 'post', 'hero', 'current', 'manchesterCity', 'home');
+INSERT INTO orders(email, cost)
+   VALUES ('test1@gmail.com', 32);
 
-INSERT INTO orders(email, status, cost, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
-	VALUES ('test2@gmail.com', 1, '25', false, 'Rowland', '17', 'bring', 'custom', 'previous', 'chelsea', 'away');
+INSERT INTO orders(email, cost)
+   VALUES ('test1@gmail.com', 29);
 
-INSERT INTO orders(email, status, cost, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
-	VALUES ('test3@gmail.com', 2, '25', false, 'Edwards', '12', 'post', 'custom', 'current', 'arsenal', 'home');
+INSERT INTO orders(email, cost)
+   VALUES ('test2@gmail.com', 27);
 
-INSERT INTO orders(email, status, cost, sleeve, kitName, kitNumber, deliveryType, printingType, style, colour, letter) 
-	VALUES ('test4@gmail.com', 0, '25', false, 'Ralph', '4', 'post', 'custom', 'current', 'black', 'straight');
+INSERT INTO orders(email, cost)
+   VALUES ('test3@gmail.com', 25);
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (1, now(), 'Waiting for shirt');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (2, now(), 'Shirt Recieved');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (2, now(), 'Waiting for shirt');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (2, now(), 'Shirt Recieved');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (2, now(), 'Shirt Sent Back');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (3, now(), 'Waiting for shirt');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (4, now(), 'Waiting for shirt');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (4, now(), 'Shirt Recieved');
+
+INSERT INTO statuses(orderNumber, dateChanged, description)
+   VALUES (5, now(), 'Waiting for shirt');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
+	VALUES (1, false, 'Kolarov', '11', 'post', 'hero', 'current', 'manchesterCity', 'home');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
+   VALUES (1, false, 'Rooney', '9', 'post', 'hero', 'current', 'everton', 'home');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
+	VALUES (2, true, 'Sterling', '7', 'post', 'hero', 'current', 'manchesterCity', 'home');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
+	VALUES (3, false, 'Rowland', '17', 'bring', 'custom', 'previous', 'chelsea', 'away');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, club, strip) 
+	VALUES (4, true, 'Edwards', '12', 'post', 'custom', 'current', 'arsenal', 'home');
+
+INSERT INTO shirts(orderNumber, sleeve, kitName, kitNumber, deliveryType, printingType, style, colour, letter) 
+	VALUES (5,false, 'Ralph', '4', 'post', 'custom', 'current', 'black', 'straight');
