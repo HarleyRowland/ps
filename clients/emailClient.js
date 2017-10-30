233e5a6de85c69dd3ffec5875a3e380eb3815e56
Harley Rowland <harleyrowland17@gmail.com>
2017-10-30 06:17:52 +0000

    Add sales email to email send list

--- clients/emailClient.js
+++ clients/emailClient.js
@@ -20,14 +20,13 @@ module.exports = {
 		send(req.query.description, email, name, cost, req.query.orderNumber, deliveryOption, deliveryDate, callback);
 	},
 	queryEmail: function(req, callback){
-		var emails = [config.email.email]
-		console.log("∆∆∆", req.query);
+		var emails = [config.email.email, "sales@premiershirts.co.uk"]
 		var subject = "New Query Request From " + req.query.email
 		var content = "<p>Query email from: " + req.query.name + " (" + req.query.email + "/" + req.query.number + ")</p><p> They said: " + req.query.comments + "</p>"
 		sendMail(emails, subject, content, callback)
 	},
 	quoteEmail: function(req, callback){
-		var emails = [config.email.email];
+		var emails = [config.email.email, "sales@premiershirts.co.uk"];
 		var subject = "New Quote Request From " + req.query.email;
 		var content = "<p>Quote email from: " + req.query.name + " (" + req.query.email + ")</p><p> The Quote: " + req.query.comments + "</p><ul><li>League: " + req.query.league + "</li><li>Club: " + req.query.club + "</li><li>Strip: " + req.query.strip + "</li><li>Year: " + req.query.year + "</li><li>Colour: " + req.query.colour + "</li><li>Letter: " + req.query.letter + "</li><li>Kit Name: " + req.query.kitName + "</li><li>Kit Number: " + req.query.kitNumber + "</li><li>Extra Comments: " + req.query.comments + "</li></ul>";
 		sendMail(emails, subject, content, callback);
@@ -37,7 +36,7 @@ module.exports = {
 var send = function(subject, toEmailAddress, name, cost, orderNumber, deliveryOption, deliveryDate, callback){
 	var emails, emailSubject, content;
 	if(subject == "Payment"){
-		emails = [toEmailAddress, config.email.authorEmail, config.email.email]
+		emails = [toEmailAddress, config.email.authorEmail, config.email.email, "sales@premiershirts.co.uk"]
 		emailSubject =  subject + " - Order Number: " + orderNumber
 		if(deliveryOption == "post") {
 			content = "<p>Hello " + name + ",</p><p>Thank you for your order. Please keep make a note of your order number(" + orderNumber + ").</p><p>The total cost for your order is £" + cost + ".</p><p>Please deliver your shirt to Suite I, 1 Elwick Road, Ashford, Kent, TN23 1PD. We will notify you when we have recieved your shirt.</p><p>Kind Regards,</p><p>The Premier Shirts Team</p>"
@@ -47,11 +46,11 @@ var send = function(subject, toEmailAddress, name, cost, orderNumber, deliveryOp
 			content = "<p>Hello " + name + ",</p><p>Thank you for your order. Please keep make a note of your order number(" + orderNumber + ").</p><p>The total cost for your order is £" + cost + ".</p><p>We will notify you when your letters are on their way.</p><p>Kind Regards,</p><p>The Premier Shirts Team</p>"		
 		}
 	} else if( subject == "Shirt Received") {
-		emails = [toEmailAddress]
+		emails = [toEmailAddress, "sales@premiershirts.co.uk"]
 		emailSubject =  subject + " - Order Number: " + orderNumber
 		content = "<p>Hello " + name + ",</p><p>We have recieved your order.</p><p>We will notify you as soon as it is on its way back to you.</p><p>Kind Regards,</p><p>The Premier Shirts Team</p>"
 	} else if(subject == "Shirt Sent Back") {
-		emails = [toEmailAddress]
+		emails = [toEmailAddress, "sales@premiershirts.co.uk"]
 		emailSubject =  subject + " - Order Number: " + orderNumber
 		content = "<p>Hello " + name + ",</p><p>Your order is on its way to you.</p><p>We Hope you are happy with everything!</p><p>Kind Regards,</p><p>The Premier Shirts Team</p>"
 	} 
