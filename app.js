123af5125391642b8bb8ff8a4f01a97b584d2d01
Harley Rowland <harleyrowland17@gmail.com>
2017-11-01 19:31:16 +0000

    Change to http

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
   app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https')
+    if (req.headers['x-forwarded-proto'] !== 'http')
       if(req.header('host').includes("www")){
         res.redirect("http://" + req.header('host') + req.url);
       } else {
